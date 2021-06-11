05131b4d6e95ad60ecde006d289c0df75085081a
Alexandro Mena <alexandrothedev@gmail.com>
2021-06-11 21:14:24 +0800

    define category Schema

---
+++ models/Category.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const CategorySchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required : [ true, "Category name required"]
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, "Category creator required"]
+    }
+})
+
+module.exports = model('Category', CategorySchema)
