4774592feffdc07662bb5b04dad4cea307283cd5
Alexandro Mena <alexandrothedev@gmail.com>
2021-06-21 15:06:29 +0800

    modify response

--- db.js
+++ db.js
@@ -4,7 +4,8 @@ module.exports = () =>{
     mongoose.connect(process.env.DB_URL, {
         useNewUrlParser: true, 
         useUnifiedTopology: true,
-        useCreateIndex: true
+        useCreateIndex: true,
+        useFindAndModify : false
     })
     .then(() => console.log("Connected to database"))
     .catch( err =>{
--- routes/records.js
+++ routes/records.js
@@ -36,7 +36,7 @@ router.post('/', verifyUser, (req,res,next)=>{
 router.get('/', verifyUser, (req,res,next) =>{
 
     Record.find({user: req.user._id})
-    .sort('-date')
+    .sort({date: 'desc',createdAt: 'desc'})
     .populate('category')
     .then( records => {
         res.send(records)
@@ -71,12 +71,13 @@ router.put('/:id', verifyUser, (req,res,next) =>{
     },req.body, {new:true})
     .then( record => {
         if(record) {
-            res.send(record)
+            return Record.findById(record._id)
         }else{
             res.status(404)
             next(new Error("Not Found"))
         }
     })
+    .then( record => res.send())
     .catch(next)
 
 })
